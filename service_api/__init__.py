669579c5ef61db1505e84c88448575af2046dfde
BodiaPavlyk <bogdan2002mih@gmail.com>
2021-03-17 21:54:43 +0200

    Created models for database

--- service_api/__init__.py
+++ service_api/__init__.py
@@ -2,13 +2,14 @@ from flask import Flask
 from flask_restful import Api
 from sqlalchemy.orm import declarative_base, sessionmaker
 import os
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, MetaData
 
 flask_app = Flask(__name__)
 api_ = Api(flask_app)
 
 engine = create_engine(os.environ["DATABASE_URL"])
-Base = declarative_base()
+metadata = MetaData()
+Base = declarative_base(metadata)
 Session = sessionmaker(bind=engine)
 
-from . import views
+from . import views, models

d1663521d7ce0ee734d102675d12eae86a65cd8c
Ostap <ostapbod674@gmail.com>
2021-03-23 18:46:23 +0200

    Redis added to requirements.txt. Initialization of redis caching object in __init__

--- service_api/__init__.py
+++ service_api/__init__.py
@@ -3,17 +3,24 @@ from flask_restful import Api
 from sqlalchemy.orm import declarative_base, sessionmaker
 import os
 from sqlalchemy import create_engine, MetaData
+import redis
 
-
+# initialisation of flask base app
 flask_app = Flask(__name__)
 flask_app.config.from_object('config.DevelopmentConfig')
+
+# creating variable for building routes using flask_restful
 api_ = Api(flask_app)
 
+# connecting to DB
 engine = create_engine(os.environ["DATABASE_URL"])
 metadata = MetaData()
 Base = declarative_base(metadata)
 Session = sessionmaker(bind=engine)
 
+# entrypoint for caching using redis
+cache = redis.Redis(
+    host=os.environ['REDIS_IP'], port=os.environ['REDIS_PORT'])
 
 from . import models
 from service_api.client_api import resources

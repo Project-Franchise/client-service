bf5f40a6f06b34895e2f95697f8df7b2649329ce
Ostap <ostapbod674@gmail.com>
2021-03-23 18:48:24 +0200

    Simple visits count logic implemented in client_service/*.py . Redis added to docker-compose.yaml

--- service_api/client_api/resources.py
+++ service_api/client_api/resources.py
@@ -1,10 +1,15 @@
 from flask_restful import Resource
-from service_api import api_
+from service_api import api_, cache
+from redis.exceptions import ConnectionError
 
 
 class IndexResource(Resource):
     def get(self):
-        return "Project Realty"
+        try:
+            count = cache.incr('hits')
+        except ConnectionError as exc:
+            return f"Redis connection error {exc}"
+        return f"Project Realty: hits: {count}"
 
 
 api_.add_resource(IndexResource, "/")

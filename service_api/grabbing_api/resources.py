2839ff45e71a7f171691afbf6e4ddbe3eda8783b
Ostap <ostapbod674@gmail.com>
2021-03-27 21:56:31 +0200

    Code refactored to follow pylint rules

--- service_api/grabbing_api/resources.py
+++ service_api/grabbing_api/resources.py
@@ -1,56 +1,80 @@
-from flask_restful import Resource
-import requests
+"""
+Resources and urls for grabbing service
+"""
 import os
-from service_api import api_, schemas, models, Session as Session_
-from . import constants
+from typing import List
+
+import requests
+from flask_restful import Resource
+from service_api import Session as Session_
+from service_api import api_, models, schemas
 from sqlalchemy.orm import Session
 
-
-# ATENTION! THIS CODE SHOULD BE REFACRORED
-# MADE WITH AIM TO PREVENT BLOCKING TASK #32
-
-
-def get_cities(state):
-    params = {
-        "lang_id": 4,
-        "api_key": os.environ['DOMRIA_API_KEY']
-    }
-
-    url = constants.DOMRIA_DOMAIN + \
-        constants.DOMRIA_URL["cities"] + f'/{state.original_id}'
-    response = requests.get(url, params=params)
-
-    cities_json = response.json()
-    city_schema = schemas.CitySchema(many=True)
-
-    processed_cities = [{"name": city["name"],
-                         "original_id": city["cityID"],
-                         "state_id": state.id}
-                        for city in cities_json]
-
-    cities = city_schema.load(processed_cities)
-
-    session: Session = Session_()
-    session.add_all(cities)
-    session.commit()
-
-    return processed_cities
+from . import constants
 
 
 class CitiesFromDomriaResource(Resource):
-    def get(self):
-        session: Session = Session_()
+    """
+    Retrieving cities from DOMRIA and saving to DB
+    """
 
+    def get(self):
+        """
+        Get all cities from all states
+        :return: list of serialized cities
+        """
+        session: Session = Session_()
         states = session.query(models.State).all()
 
-        return [get_cities(state) for state in states]
+        return [self.get_cities_by_state(state) for state in states]
+
+    @staticmethod
+    def get_cities_by_state(state: models.State) -> List[dict]:
+        """
+        Getting cities from DOMRIA by original state id.
+        Returns list of serialized cities
+        :param: state: State
+        :return: List[dict]
+        """
+        params = {
+            "lang_id": constants.DOMRIA_UKR,
+            "api_key": constants.DOMRIA_API_KEY
+        }
+
+        url = constants.DOMRIA_DOMAIN + \
+            constants.DOMRIA_URL["cities"] + f'/{state.original_id}'
+        response = requests.get(url, params=params)
+
+        cities_json = response.json()
+        city_schema = schemas.CitySchema(many=True)
+
+        processed_cities = [{"name": city["name"],
+                             "original_id": city["cityID"],
+                             "state_id": state.id}
+                            for city in cities_json]
+
+        cities = city_schema.load(processed_cities)
+
+        session: Session = Session_()
+        session.add_all(cities)
+        session.commit()
+
+        return processed_cities
 
 
 class StatesFromDomriaResource(Resource):
+    """
+    Retrieving states from DOMRIA and saving to DB
+    """
+
     def get(self):
+        """
+        Get all states from all states
+        :return: list of serialized cities
+        """
         params = {
-            "lang_id": 4,
-            "api_key": os.environ['DOMRIA_API_KEY']
+            "lang_id": constants.DOMRIA_UKR,
+            "api_key": constants.DOMRIA_API_KEY
         }
         response = requests.get(constants.DOMRIA_DOMAIN +
                                 constants.DOMRIA_URL["states"], params=params)
@@ -69,6 +93,7 @@ class StatesFromDomriaResource(Resource):
         session.commit()
 
         return processed_states
+
 
 # Be careful. Use this lisnks only once!!
 api_.add_resource(StatesFromDomriaResource, "/get/states")

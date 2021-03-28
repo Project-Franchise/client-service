808590f53ae5d1996f51c4f4d5dfb2e254c544c5
irayarka <iryna.yarka@gmail.com>
2021-03-28 17:52:34 +0300

    Added route for 1 particular City, all realties, realty by state and realty by city

--- service_api/client_api/resources.py
+++ service_api/client_api/resources.py
@@ -1,38 +1,6 @@
 from flask_restful import Resource
 from service_api import api_, Session, models, schemas
 from contextlib import contextmanager
-from flask import jsonify
-
-
-Chars = {
-    'Location': {
-        'State': [1, 2, 3, 4],
-        'City': [1, 2, 3, 4]
-    },
-    'Realty': {
-        'floor': {
-            'from': 'number',
-            'to': 'number'
-        },
-        'floors_number': {
-            'from': 'number',
-            'to': 'number'
-        },
-        'square': {
-            'from': 'number',
-            'to': 'number'
-        },
-        'rental_price': {
-            'from': 'number',
-            'to': 'number'
-        },
-        'sale_price': {
-            'from': 'number',
-            'to': 'number'
-        },
-        'building_state': 'string'
-    }
-}
 
 
 @contextmanager
@@ -57,19 +25,49 @@ class IndexResource(Resource):
         return "Project Realty"
 
 
+class City(Resource):
+    def get(self, state_id, id):
+        with session_scope() as session:
+            session = Session()
+            city = session.query(models.City).filter_by(state_id=state_id, id=id).first()
+        return schemas.CitySchema().dump(city)
+
+
 class Cities(Resource):
     def get(self, state_id):
         with session_scope() as session:
             session = Session()
             cities = session.query(models.City).filter_by(state_id=state_id).all()
-        return jsonify(schemas.CitySchema(many=True).dump(cities))
+        return schemas.CitySchema(many=True).dump(cities)
 
 
-class Characteristics(Resource):
+class Realty(Resource):
     def get(self):
+        with session_scope() as session:
+            session = Session()
+            realties = session.query(models.Realty).filter_by().all()
+        return schemas.RealtySchema(many=True).dump(realties)
 
-        return Chars
+
+class RealtyByCity(Resource):
+    def get(self, state_id, city_id):
+        with session_scope() as session:
+            session = Session()
+            realties = session.query(models.Realty).filter_by(state_id=state_id, city_id=city_id).all()
+        return schemas.RealtySchema(many=True).dump(realties)
+
+
+class RealtyByState(Resource):
+    def get(self, state_id):
+        with session_scope() as session:
+            session = Session()
+            realties = session.query(models.Realty).filter_by(state_id=state_id).all()
+        return schemas.RealtySchema(many=True).dump(realties)
 
 
 api_.add_resource(IndexResource, "/")
 api_.add_resource(Cities, '/cities/<state_id>')
+api_.add_resource(City, '/cities/<state_id>/<id>')
+api_.add_resource(Realty, '/realty')
+api_.add_resource(RealtyByState, '/realty/<state_id>')
+api_.add_resource(RealtyByCity, '/realty/<state_id>/<city_id>')

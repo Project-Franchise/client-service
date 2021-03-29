20ba85ce9894b5b01b8166066251fe07e6c53f71
BurlyLlama28 <kiltik12@gmail.com>
2021-03-29 21:39:10 +0300

    refactored code and added doctsrings

--- service_api/grabbing_api/characteristics.py
+++ service_api/grabbing_api/characteristics.py
@@ -5,35 +5,43 @@ import requests
 
 import constants
 
-from pprint import pprint
-
 
 def decode_characteristics(dct: Dict) -> Dict:
+    """
+    Custom object hook.
+    Used for finding characteristics
+    in "items" dict
+    """
     item_list = {}
-    if not 'items' in dct:
+    if not "items" in dct:
         return dct
-    if 'items' in dct:
-        for fields in dct['items']:
-            if 'field_name' in fields:
+    if "items" in dct:
+        for fields in dct["items"]:
+            if "field_name" in fields:
                 item_list.update(
-                    {fields['field_name']: fields['characteristic_id']})
+                    {fields["field_name"]: fields["characteristic_id"]})
         return item_list
     return dct
 
 
 def get_characteristics(characteristics: Dict = dict()) -> Dict:
-    with open('service_api\static data\main_hardcode.json') as json_file:
+    """
+    Function to get characteristics 
+    and retrieve them in dict
+    """
+    with open("service_api\static data\main_hardcode.json") as json_file:
         сharacteristics_data_set = json.load(json_file)
-    for element in сharacteristics_data_set['realty_type']:
+    for element in сharacteristics_data_set["realty_type"]:
         req = requests.get(
             constants.DOMRIA_DOMAIN + constants.DOMRIA_URL["options"],
-            params={"realty_type": сharacteristics_data_set['realty_type'][element],
+            params={"realty_type": сharacteristics_data_set["realty_type"][element],
                     "operation_type": 1,
                     "api_key": constants.DOMRIA_API_KEY})
-        listik = req.json(object_hook=decode_characteristics)
-        listik = [element for element in listik if element != {}]
-        dictik = {}
-        for i in listik:
-            dictik.update(i)
-        characteristics.update({element: dictik})
+        list_of_characteristics = req.json(object_hook=decode_characteristics)
+        list_of_characteristics = [
+            element for element in list_of_characteristics if element != {}]
+        dict_of_characteristics = {}
+        for i in list_of_characteristics:
+            dict_of_characteristics.update(i)
+        characteristics.update({element: dict_of_characteristics})
     return characteristics

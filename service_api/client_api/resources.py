1d54869c68ab54732b21a62635adc4903e68dcfa
Ostap <ostapbod674@gmail.com>
2021-03-29 23:22:35 +0300

    Fixed files after merge

--- service_api/client_api/resources.py
+++ service_api/client_api/resources.py
@@ -1,43 +1,20 @@
-from flask_restful import Resource
-<<<<<<< Updated upstream
-from redis.exceptions import ConnectionError
-from service_api import api_, cache
-=======
-<<<<<<< Updated upstream
-from service_api import api_
-=======
-from redis.exceptions import ConnectionError
 from service_api import api_, CACHE
->>>>>>> Stashed changes
->>>>>>> Stashed changes
+from redis.exceptions import ConnectionError
+from flask_restful import Resource
 
 
 class IndexResource(Resource):
     """Main View entity based on Resource(from flask_restful"""
 
     def get(self):
-<<<<<<< Updated upstream
-=======
-<<<<<<< Updated upstream
-        return "Project Realty"
-=======
->>>>>>> Stashed changes
         """HTTP GET method realisation.
         :return: str
         """
         try:
-<<<<<<< Updated upstream
-            count = cache.incr('hits')
+            count = CACHE.incr("hits")
         except ConnectionError as exc:
             return f"Redis connection error {exc}"
         return f"Project Realty: hits: {count}"
-=======
-            count = CACHE.incr('hits')
-        except ConnectionError as exc:
-            return f"Redis connection error {exc}"
-        return f"Project Realty: hits: {count}"
->>>>>>> Stashed changes
->>>>>>> Stashed changes
 
 
 api_.add_resource(IndexResource, "/")

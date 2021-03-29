6e0f46f2038c2e8632a804c51df257f5305f1efd
Andrii Mruts <cotyhoroshko@gmail.com>
2021-03-29 21:05:18 +0300

    Added constrains

--- service_api/grabbing_api/constants.py
+++ service_api/grabbing_api/constants.py
@@ -1,4 +1,8 @@
+from service_api import models
+
 DOMRIA_DOMAIN = "https://developers.ria.com/dom"
+PAGE_ADS_NUMBER = 10
+
 DOMRIA_URL = {
     "cities": "/cities",
     "states": "/states",
@@ -6,3 +10,24 @@ DOMRIA_URL = {
     "search": "/search",
     "id": "/info"
 }
+
+REALTY_KEYS = [
+    ("city_id", models.City, "city_id"),
+    ("state_id", models.State, "state_id"),
+    ("realty_details_id", models.RealtyDetails, "realty_id"),
+    ("realty_type_id", models.RealtyType, "realty_type_id"),
+    ("operation_type_id", models.OperationType, "advert_type_id")
+
+]
+
+REALTY_DETAILS_KEYS = {
+    "floor": "floor",
+    "floors_number": "floors_count",
+    "square": "total_square_meters",
+    "price": "price",
+    "published_at": "publishing_date",
+    "original_id": "realty_id",
+    "original_url": "beautiful_url",
+}
+
+

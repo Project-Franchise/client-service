c94cc3e82636ce72a928fa7fd1ffe5f9cc9124f8
Andrii Mruts <cotyhoroshko@gmail.com>
2021-03-29 21:45:43 +0300

    Create the logic of converting data from DomRia

--- service_api/grabbing_api/utils/grabbing_utils.py
+++ service_api/grabbing_api/utils/grabbing_utils.py
@@ -1,8 +1,14 @@
-from typing import Dict
+import os
+import sys
+from typing import List, Dict
 
+import requests
 from marshmallow import ValidationError
 from marshmallow.schema import SchemaMeta
 from sqlalchemy.orm import Session
+
+sys.path.append(os.getcwd())
+from service_api import session
 
 
 def load_data(data: Dict, session: Session, ModelSchema: SchemaMeta) -> SchemaMeta:
@@ -16,3 +22,26 @@ def load_data(data: Dict, session: Session, ModelSchema: SchemaMeta) -> SchemaMe
     session.add(res_data)
     session.commit()
     return res_data
+
+
+def make_realty_details_data(response: requests.models.Response, realty_details_keys: Dict) -> Dict:
+    original_keys = [response.json()[val] for val in realty_details_keys.values()]
+    self_keys = realty_details_keys.keys()
+
+    realty_details_data = {
+        key: value for (key, value) in
+        zip(self_keys, original_keys)
+    }
+
+    return realty_details_data
+
+
+def make_realty_data(response: requests.models.Response, realty_keys: List) -> Dict:
+    realty_data = dict()
+    for keys in realty_keys:
+        id, model, response_key = keys
+        realty_data[id] = (session.query(model).filter(
+            model.original_id == response.json()[response_key]
+        ).first()).id
+
+    return realty_data

640a1510f213b5f1a376c6dccb715ba7c8bf043c
irayarka <iryna.yarka@gmail.com>
2021-03-30 12:54:16 +0300

    Routes for realty changed

--- service_api/client_api/resources.py
+++ service_api/client_api/resources.py
@@ -1,21 +1,25 @@
 from flask_restful import Resource
 from service_api import api_, Session, models, schemas
 from contextlib import contextmanager
+from typing import Iterator
+from sqlalchemy.exc import SQLAlchemyError
+from flask import request
+import requests
 
 
 @contextmanager
-def session_scope():
+def session_scope() -> Iterator[Session]:
     session = Session()
     try:
         yield session
         session.commit()
-    except:
+    except SQLAlchemyError:
         session.rollback()
         raise
     else:
         try:
             session.commit()
-        except:
+        except SQLAlchemyError:
             session.rollback()
             raise
 
@@ -25,112 +29,92 @@ class IndexResource(Resource):
         return "Project Realty"
 
 
-class City(Resource):
+class CityResource(Resource):
     def get(self, state_id, id):
         with session_scope() as session:
-            session = Session()
             city = session.query(models.City).filter_by(state_id=state_id, id=id).first()
         return schemas.CitySchema().dump(city)
 
 
-class Cities(Resource):
+class CitiesResource(Resource):
     def get(self, state_id):
         with session_scope() as session:
-            session = Session()
             cities = session.query(models.City).filter_by(state_id=state_id).all()
         return schemas.CitySchema(many=True).dump(cities)
 
 
-class States(Resource):
+class StatesResource(Resource):
     def get(self):
         with session_scope() as session:
-            session = Session()
             states = session.query(models.State).filter_by().all()
         return schemas.StateSchema(many=True).dump(states)
 
 
-class State(Resource):
+class StateResource(Resource):
     def get(self, id):
         with session_scope() as session:
-            session = Session()
             state = session.query(models.State).filter_by(id=id).first()
         return schemas.StateSchema().dump(state)
 
 
-class Realty(Resource):
+# bug: filters must be validated
+class RealtyFromDBResource(Resource):
     def get(self):
+        filters = request.args
+        realty_schema = schemas.RealtySchema()
         with session_scope() as session:
-            session = Session()
-            realties = session.query(models.Realty).filter_by().all()
-        return schemas.RealtySchema(many=True).dump(realties)
+            realty = session.query(models.Realty).join(models.RealtyDetails, models.Realty.realty_details_id ==
+                                                       models.RealtyDetails.id).filter_by(**filters).all()
+        return realty_schema.dump(realty, many=True)
 
 
-class RealtyByCity(Resource):
-    def get(self, state_id, city_id):
-        with session_scope() as session:
-            session = Session()
-            realties = session.query(models.Realty).filter_by(state_id=state_id, city_id=city_id).all()
-        return schemas.RealtySchema(many=True).dump(realties)
-
-
-class RealtyByState(Resource):
-    def get(self, state_id):
-        with session_scope() as session:
-            session = Session()
-            realties = session.query(models.Realty).filter_by(state_id=state_id).all()
-        return schemas.RealtySchema(many=True).dump(realties)
-
-
-class RealtyDetails(Resource):
-    def get(self, realty_details_id):
-        with session_scope() as session:
-            session = Session()
-            realty = session.query(models.Realty).filter_by(realty_details_id=realty_details_id).first()
-            details = session.query(models.RealtyDetails).filter_by(id=realty.realty_details_id).first()
-        return schemas.RealtyDetailsSchema().dump(details)
-
-
-class RealtyTypes(Resource):
+class RealtyFromGrabbingResource(Resource):
     def get(self):
+        filters = request.args
+
+        response = requests.get('', params=filters)
+        return response.json()
+
+
+class RealtyTypesResource(Resource):
+    def get(self):
+        filters = request.args
+        if int(filters["id"]) < 0:
+            return {'error': 'invalid id'}
         with session_scope() as session:
-            session = Session()
-            realty_types = session.query(models.RealtyType).filter_by().all()
+            realty_types = session.query(models.RealtyType).filter_by(**filters).all()
         return schemas.RealtyTypeSchema(many=True).dump(realty_types)
 
 
-class RealtyType(Resource):
+class RealtyTypeResource(Resource):
     def get(self, realty_type_id):
         with session_scope() as session:
-            session = Session()
             realty_type = session.query(models.RealtyType).filter_by(id=realty_type_id).first()
         return schemas.RealtyTypeSchema().dump(realty_type)
 
 
-class OperationTypes(Resource):
+class OperationTypesResource(Resource):
     def get(self):
         with session_scope() as session:
-            session = Session()
             operation_types = session.query(models.OperationType).filter_by().all()
         return schemas.OperationTypeSchema(many=True).dump(operation_types)
 
 
-class OperationType(Resource):
+class OperationTypeResource(Resource):
     def get(self, operation_type_id):
         with session_scope() as session:
-            session = Session()
             operation_type = session.query(models.OperationType).filter_by(id=operation_type_id).first()
         return schemas.OperationTypeSchema().dump(operation_type)
 
+
 api_.add_resource(IndexResource, "/")
-api_.add_resource(Cities, '/cities/<state_id>')
-api_.add_resource(City, '/cities/<state_id>/<id>')
-api_.add_resource(Realty, '/realty')
-api_.add_resource(RealtyByState, '/realty/<state_id>')
-api_.add_resource(RealtyByCity, '/realty/<state_id>/<city_id>')
-api_.add_resource(States, '/states/')
-api_.add_resource(State, '/states/<id>')
-api_.add_resource(RealtyDetails, '/details/<realty_details_id>')
-api_.add_resource(RealtyTypes, '/realty_types/')
-api_.add_resource(RealtyType, '/realty_type/<realty_type_id>')
-api_.add_resource(OperationTypes, '/operation_types/')
-api_.add_resource(OperationType, '/operation_type/<operation_type_id>')
+api_.add_resource(CitiesResource, '/cities/<state_id>')
+api_.add_resource(CityResource, '/cities/<state_id>/<id>')
+api_.add_resource(RealtyFromDBResource, '/realty_db')
+api_.add_resource(RealtyFromGrabbingResource, '/realty_gb')
+api_.add_resource(StatesResource, '/states/')
+api_.add_resource(StateResource, '/states/<id>')
+api_.add_resource(RealtyTypesResource, '/realty_types/')
+api_.add_resource(RealtyTypeResource, '/realty_type/<realty_type_id>')
+api_.add_resource(OperationTypesResource, '/operation_types/')
+api_.add_resource(OperationTypeResource, '/operation_type/<operation_type_id>')

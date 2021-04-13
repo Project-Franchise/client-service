330654258be18c4a9cd6445f85d8eb3ca50affa5
Andrii Mruts <cotyhoroshko@gmail.com>
2021-04-13 17:23:27 +0300

    Refactored grabbing_utils.py

--- service_api/grabbing_api/utils/grabbing_utils.py
+++ service_api/grabbing_api/utils/grabbing_utils.py
@@ -3,31 +3,34 @@ Utilities for creating models and saving them in DB
 """
 
 import json
-from typing import Dict, List
+from typing import Dict, List, Union
 
 import requests
 from marshmallow import ValidationError
 from marshmallow.schema import SchemaMeta
-from service_api.errors import BadRequestException
-from service_api.grabbing_api.constants import DOMRIA_TOKEN, PATH_TO_METADATA
+
 from service_api import models, session_scope, Base
+from service_api.errors import BadRequestException
+from service_api.grabbing_api.constants import DOMRIA_TOKEN
 from service_api.schemas import RealtyDetailsSchema, RealtySchema
 
 
-def load_data(data: Dict, model: Base, model_schema: SchemaMeta) -> SchemaMeta:
+def load_data(data: Union[Dict, List], model: Base, model_schema: SchemaMeta) -> SchemaMeta:
     """
     Stores data in a database according to a given scheme
     """
     try:
-        valid_data = model_schema().load(data)
-        record = model(**valid_data)
+        if isinstance(data, dict):
+            data = [data]
+        valid_data = model_schema(many=True).load(data)
+        record = [model(**data) for data in valid_data]
     except ValidationError as error:
         raise BadRequestException(error.args) from error
 
     with session_scope() as session:
-        session.add(record)
+        session.add_all(record)
         session.commit()
-    return record
+    return record[0]
 
 
 def make_realty_details_data(response: requests.models.Response, realty_details_meta: Dict) -> Dict:
@@ -37,8 +40,8 @@ def make_realty_details_data(response: requests.models.Response, realty_details_
 
     data = response.json()
 
-    values = [data.get(val, None) for val in realty_details_meta.values()]
     keys = realty_details_meta.keys()
+    values = [data.get(val["response_key"], None) for val in realty_details_meta.values()]
 
     realty_details_data = dict(zip(
         keys, values
@@ -57,14 +60,11 @@ def make_realty_data(response: requests.models.Response, realty_keys: Dict) -> D
             model = characteristics["model"]
             response_key = characteristics["response_key"]
 
-            # if model not in [subclass.__name__ for subclass in Base.__subclasses__()]:
-            #     raise Warning(f"There is no such model named {model}")
             model = getattr(models, model)
 
             if not model:
                 raise Warning(f"There is no such model named {model}")
 
-            # model = eval(f"models.{model}")
             realty_data[key] = (session.query(model).filter(
                 model.original_id == response.json()[response_key]
             ).first()).id  # and service_name == service_name
@@ -116,7 +116,7 @@ def create_records(id_list: List, service_metadata: Dict) -> List[Dict]:
     return realty_models
 
 
-def process_request(search_response: Dict, page: int, page_ads_number: int, service_name: str) -> List[Dict]:
+def process_request(search_response: Dict, page: int, page_ads_number: int, metadata: Dict) -> List[Dict]:
     """
     Distributes a list of ids to write to the database and return to the user
     """
@@ -125,25 +125,7 @@ def process_request(search_response: Dict, page: int, page_ads_number: int, serv
                     page * page_ads_number - page_ads_number: page * page_ads_number
                     ]
 
-    # >>>>>>>>>>>>>>>>>>>>> Get metadata and check if this service exists | Single function after
-    try:
-        with open(PATH_TO_METADATA) as meta_file:
-            metadata = json.load(meta_file)
-    except json.JSONDecodeError as err:
-        print(err)
-        raise
-    except FileNotFoundError:
-        print("Invalid metadata path, or metadata.json file does not exist")
-        raise
-
-    if service_name not in metadata:
-        raise KeyError("Invalid service name")
-
-    service_metadata = metadata[service_name]
-
-    # <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< gmt end
-
-    return create_records(current_items, service_metadata)
+    return create_records(current_items, metadata)
 
 
 def open_metadata(path: str) -> Dict:

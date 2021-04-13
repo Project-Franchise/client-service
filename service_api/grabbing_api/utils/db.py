c0f167c95881e98ff82653f4ce39a803bd1af69d
Ostap <ostapbod674@gmail.com>
2021-04-14 01:27:50 +0300

    Cities Loader implementation added

--- service_api/grabbing_api/utils/db.py
+++ service_api/grabbing_api/utils/db.py
@@ -1,15 +1,17 @@
+"""
+Module with data Loaders
+"""
 from abc import ABC, abstractmethod
+
 import requests
-from service_api.schemas import CitySchema
+from requests.exceptions import RequestException
+from service_api import session_scope
+from service_api.grabbing_api.constants import DOMRIA_TOKEN, PATH_TO_METADATA
 from service_api.models import City, State
-from marshmallow.exceptions import ValidationError
-from service_api.errors import BadRequestException
-from service_api import session_scope, CACHE
-from typing import List, Dict, Any
-from service_api.grabbing_api.constants import REDIS_CITIES_FETCHED, REDIS_STATES_FETCHED
-import pickle
-import json
-import itertools
+from service_api.schemas import CitySchema
+
+from .grabbing_utils import load_data, open_metadata
+
 
 class BaseLoader(ABC):
     """
@@ -17,107 +19,54 @@ class BaseLoader(ABC):
     """
 
     @abstractmethod
-    @property
-    def name(self):
-        """
-        Name of this class
-        Is used in metadata
-        """
-        pass
-
-    @abstractmethod
-    def load_to_db(self) -> None:
+    def load_to_db(self, **kwargs) -> None:
         """
         Main function of retrieving and loading data to db
         """
-        pass
+        return None
 
 
+class CityLoader(BaseLoader):
+    """
+    Loads cities to db
+    """
 
-class LoadCitiesToDB(BaseLoader):
-    __name = "cities_loader"
-
-    @property
-    def name(self):
-        return self.__name
-
-    def load_to_db(self) -> Dict:
+    def load_to_db(self, **kwargs) -> int:
         """
-        Get all cities from all states
-        :return: list of serialized cities
+        Getting cities from DOMRIA by original state_id
+        Returns amount of fetched cities
+        :param: state_id: int
+        :return: int
         """
-        cached_sates_status = CACHE.get(REDIS_STATES_FETCHED)
+
+        if state_id := kwargs.get("state_id"):
+            raise KeyError("No parameter state_id provided in function load_to_db")
+
         with session_scope() as session:
-            number_of_cities_by_state = session.query(City).group_by(City.state_id).order_by(City.state_id).count()
+            state = session.query(State).get({"id": state_id})
 
-        if cached_sates_status is not None and \
-           json.loads(cached_sates_status) == number_of_cities_by_state:
+        domria_meta = open_metadata(PATH_TO_METADATA)["DOMRIA API"]
+        domria_cities_meta = domria_meta["url_rules"]["cities"]
 
-            city_schema = CitySchema(many=True)
-
-            cached_status = CACHE.get(REDIS_CITIES_FETCHED)
-            if cached_status is not None and pickle.loads(cached_status):
-                with session_scope() as session:
-                    cities = session.query(City).all()
-
-                return {
-                    "status": "Allready in db",
-                    "data": city_schema.dump(cities)
-                }
-
-            with session_scope() as session:
-                states = session.query(State).order_by(State.id).all()
-
-            for cities_by_state in (self.__get_cities_by_state(state) for state in states):
-
-            cities = list(itertools.chain.from_iterable(city_generator))
-
-
-            try:
-                CACHE.set(REDIS_CITIES_FETCHED, json.dumps(True))
-            except RedisError as error:
-                raise RedisError(error.args)
-
-            return {
-                "status": "fetched from domria",
-                "data": cities
-            }
-
-        raise BadRequestException("There is no state in db")
-
-    def __get_cities_by_state(self, state) -> List[Dict[str, Any]]:
-        """
-        Getting cities from DOMRIA by original state id.
-        Returns list of serialized cities
-        :param: state: State
-        :return: List[dict]
-        """
         params = {
-            "lang_id": DOMRIA_UKR,
-            "api_key": DOMRIA_API_KEY
+            "lang_id": domria_meta["optional"]["lang_id"],
+            "api_key": DOMRIA_TOKEN
         }
 
-        url = DOMRIA_DOMAIN + \
-            DOMRIA_URL["cities"] + f"/{state.original_id}"
+        url = "{}{}/{}".format(domria_meta["base_url"], domria_cities_meta["url_prefix"], state.original_id)
         response = requests.get(url, params=params)
-
+        if not response.ok:
+            raise RequestException(response.text)
         cities_json = response.json()
 
-        try:
-            processed_cities = [{"name": city["name"],
-                                 "original_id": city["cityID"],
-                                 "state_id": state.id}
-                                for city in cities_json]
-        except KeyError:
-            return []
+        processed_cities = []
+        for city in cities_json:
+            processed_city = {key: city[external_service_key]
+                              for key, external_service_key in domria_cities_meta["filters"].items()}
+            processed_city["state_id"] = state.id
+            processed_cities.append(processed_city)
 
-        try:
-            valid_data = CitySchema(many=True).load(processed_cities)
-            cities = [City(**valid_city) for valid_city in valid_data]
-        except ValidationError:
-            raise BadRequestException("Validation failed")
+        for data in processed_cities:
+            load_data(data, City, CitySchema)
 
-        with session_scope() as session:
-            session.add_all(cities)
-
-        return processed_cities
+        return len(processed_cities)

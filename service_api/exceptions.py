1deb9cf35b010f2e1a247f8d76c00c4b61b61172
Ostap <ostapbod674@gmail.com>
2021-04-15 21:33:49 +0300

    MetadataError and CycleReferenceException added. InernalServerErrorException registered to flask error handlers

--- service_api/exceptions.py
+++ service_api/exceptions.py
@@ -2,27 +2,44 @@
 Custom exceptions for service_api
 """
 
+
 class BaseCustomException(Exception):
     """
     Base class for class custom exceptions
     """
 
-    def __init__(self, message, errors=None) -> None:
+    def __init__(self, message, desc="No decription provided") -> None:
         super().__init__(message)
-        self.errors = errors or []
+        self.desc = desc
 
 
-class ResponseNotOk(BaseCustomException):
+class ResponseNotOkException(BaseCustomException):
     """
     Exception shoud be raised when wen response from external service is not OK (status code ≥ 400)
     """
     ...
 
 
-class ObjectNotFound(BaseCustomException):
+class ObjectNotFoundException(BaseCustomException):
     """
     Exception which should be used when sqlaclhemy returns None from get function
     """
 
-    def __init__(self, message="Object not found", errors=None) -> None:
-        super().__init__(message, errors)
+    def __init__(self, *args, message="Object not found", **kwargs) -> None:
+        super().__init__(message, *args, **kwargs)
+
+
+class CycleReferenceException(BaseCustomException):
+    """
+    Raised when there is a cycle in dependencies
+    """
+    ...
+
+
+class MetaDataError(BaseCustomException):
+    """
+    Raised when there is some problems with metadata
+    """
+
+    def __init__(self, *args, message="Metadata error occured",  **kwargs) -> None:
+        super().__init__(message, *args, **kwargs)

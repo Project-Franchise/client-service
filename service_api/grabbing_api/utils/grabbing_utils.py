f9a908bebb3956dbfb171075b3ba568efad04230
BurlyLlama28 <kiltik12@gmail.com>
2021-04-22 21:32:58 +0300

    Function load_data re:build added

--- service_api/grabbing_api/utils/grabbing_utils.py
+++ service_api/grabbing_api/utils/grabbing_utils.py
@@ -3,35 +3,110 @@ Utilities for creating models and saving them in DB
 """
 
 import json
+from functools import singledispatch
 from typing import Dict, List, Union
 
 import requests
 from marshmallow import ValidationError
-from marshmallow.schema import SchemaMeta
-
-from service_api import models, session_scope, Base
+from marshmallow.schema import Schema
+from service_api import Base, models, session_scope
+from service_api.models import Realty, RealtyDetails
 from service_api.errors import BadRequestException
+from service_api.exceptions import AlreadyInDbException
 from service_api.grabbing_api.constants import DOMRIA_TOKEN
 from service_api.schemas import RealtyDetailsSchema, RealtySchema
+import datetime
 
-
-def load_data(data: Union[Dict, List], model: Base, model_schema: SchemaMeta) -> SchemaMeta:
+@singledispatch
+def load_data(model_schema: Schema, data: Dict, model: Base) -> Base:
     """
     Stores data in a database according to a given scheme
     """
     try:
-        if isinstance(data, dict):
-            data = [data]
-        valid_data = model_schema(many=True).load(data)
-        record = [model(**data) for data in valid_data]
+        valid_data = model_schema.load(data)
+        record = model(**valid_data)
     except ValidationError as error:
-        raise BadRequestException(error.args) from error
+        print(error)
+        raise
 
     with session_scope() as session:
-        session.add_all(record)
-        session.commit()
-    return record[0]
+        session.add(record)
+    return record
 
+@load_data.register
+def _(model_schema: RealtyDetailsSchema, data: Dict, model: RealtyDetails):
+    try:
+        valid_data = model_schema.load(data)
+        realty_details_record = model(**valid_data)
+        
+    except ValidationError as error:
+        print(error)
+        raise
+    with session_scope() as session:
+        realty_details = session.query(model).filter_by(
+            original_id=realty_details_record.original_id, version=realty_details_record.version).first()
+
+    if realty_details is not None:
+        print("version: ", realty_details.version)
+        incoming_data = model_schema.dump(realty_details_record)
+        db_data = model_schema.dump(realty_details)
+        incoming_data.pop("id")
+        db_data.pop("id")
+        if incoming_data == db_data:
+            raise AlreadyInDbException
+        with session_scope() as session:
+            print("they aren't equal")
+            session.expire_on_commit = False
+            session.query(model).filter_by(
+                original_id=realty_details.original_id, version=realty_details.version).update(
+                {"version": datetime.datetime.now()})
+
+            session.add(realty_details_record)
+            session.commit()
+            realty_record = session.query(Realty).filter_by(
+                realty_details_id=realty_details.id).first()
+            new_realty_details_id = session.query(model).filter_by(
+                original_id=realty_details_record.original_id, version=realty_details_record.version).first().id
+
+            session.expunge(realty_record)
+            del realty_record.id
+            realty_record.realty_details_id = new_realty_details_id
+            
+            print("needed id: ", model_schema.dump(realty_details).get("id"))
+            session.query(Realty).filter_by(
+                realty_details_id=model_schema.dump(realty_details).get("id")).update(
+                {"version": datetime.datetime.now()})
+            
+            session.add(realty_record)
+
+        with session_scope() as session:
+            session.query(Realty).filter_by(
+                realty_details_id=model_schema.dump(realty_details).get("id")).update(
+                {"version": datetime.datetime.now()})
+    with session_scope() as session:
+        session.add(realty_details_record)
+    return realty_details_record
+
+
+@load_data.register
+def _(model_schema: RealtySchema, data: Dict, model: Realty):
+    try:
+        valid_data = model_schema.load(data)
+        record = model(**valid_data)
+        print("record.realty_details_id: ", record.realty_details_id)
+    except ValidationError as error:
+        print(error)
+        # raise
+    with session_scope() as session:
+        stuff = session.query(model).filter_by(realty_details_id=record.realty_details_id,
+                                               version=record.version).first()
+        print("stuff: ", stuff)
+    if stuff is not None:
+        print("version: ", stuff.version)
+        raise AlreadyInDbException
+    with session_scope() as session:
+        session.add(record)
+    return record
 
 def make_realty_details_data(response: requests.models.Response, realty_details_meta: Dict) -> Dict:
     """
@@ -100,7 +175,12 @@ def create_records(ids: List, service_metadata: Dict) -> List[Dict]:
             print(error)
             raise
 
-        load_data(realty_details_data, models.RealtyDetails, RealtyDetailsSchema)
+        try:
+            load_data(RealtyDetailsSchema(), realty_details_data, RealtyDetails)
+        except AlreadyInDbException as error:
+            print(error)
+            continue
+            # raise
 
         try:
             realty_data = make_realty_data(response, service_metadata["model_characteristics"]["realty_columns"])
@@ -108,8 +188,12 @@ def create_records(ids: List, service_metadata: Dict) -> List[Dict]:
             print(error)
             raise
 
-        realty = load_data(realty_data, models.Realty, RealtySchema)
-
+        try:
+            realty = load_data(RealtySchema(), realty_data, Realty)
+        except AlreadyInDbException as error:
+            print(error)
+            continue
+            # raise
         schema = RealtySchema()
         elem = schema.dump(realty)
 
@@ -124,8 +208,8 @@ def process_request(search_response: Dict, page: int, page_ads_number: int, meta
     """
     page = page % page_ads_number
     current_items = search_response["items"][
-                    (page + 1) * page_ads_number - page_ads_number: (page + 1) * page_ads_number
-                    ]
+        (page + 1) * page_ads_number - page_ads_number: (page + 1) * page_ads_number
+    ]
 
     return create_records(current_items, metadata)
 
